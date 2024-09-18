c8e59372be02cc8451f57f6868d0eb3ba91bcd6d
Siddharth More <simore@microsoft.com>
2024-09-18 07:27:32 -0700

    Install GPU Operator

--- scripts/node-setup.sh
+++ scripts/node-setup.sh
@@ -13,32 +13,22 @@ function install_prereq_packages() {
 
 function install_docker() {
     # Install Docker
-    # Instructions copied from: https://docs.docker.com/engine/install/ubuntu/
-
-    # Add Docker's official GPG key:
     sudo apt-get update || true
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
         sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update || true
-
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo docker run hello-world
     sudo usermod -aG docker $USER
 }
 
 function install_kind() {
-    # Install Kind
-    # Instructions copied from: https://kind.sigs.k8s.io/docs/user/quick-start/#installation
-
-    # For AMD64 / x86_64
     pushd $(mktemp -d)
     [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
     chmod +x ./kind
@@ -47,40 +37,50 @@ function install_kind() {
 }
 
 function install_kubectl() {
-    # Install kubectl
-    # Instructions copied from: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
-
-    # If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
     sudo mkdir -p -m 755 /etc/apt/keyrings
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor | sudo tee /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null
-    sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
-
-    # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+    sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-    sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list # helps tools such as command-not-found to work correctly
-
+    sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
     sudo apt-get update || true
     sudo apt-get install -y kubectl
 }
 
 function install_helm() {
-    # Install Helm
-    # Instructions copied from: https://helm.sh/docs/intro/install/#from-apt-debianubuntu
+    if ! command -v helm &>/dev/null; then
+        curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
+        sudo apt-get install apt-transport-https --yes
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+        sudo apt-get update || true
+        sudo apt-get install -y helm
+    else
+        echo "Helm is already installed"
+    fi
+}
 
-    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
-    sudo apt-get install apt-transport-https --yes
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-    sudo apt-get update || true
-    sudo apt-get install -y helm
+function install_gpu_operator() {
+    helm repo add nvidia https://helm.ngc.nvidia.com/nvidia &&
+        helm repo update
+
+    helm install --wait --generate-name \
+        -n gpu-operator --create-namespace \
+        nvidia/gpu-operator \
+        --set mig.strategy=single \
+        --set migManager.env[0].name=WITH_REBOOT \
+        --set-string migManager.env[0].value=true
+
+    kubectl patch clusterpolicies.nvidia.com/cluster-policy \
+        --type='json' \
+        -p='[{"op":"replace", "path":"/spec/mig/strategy", "value":"single"}]'
+
+    kubectl label $(kubectl get nodes -l agentpool=mignode -o name) nvidia.com/mig.config=all-1g.10gb --overwrite
 }
 
 function install_nvidia_toolkit() {
-    # Installing the NVIDIA Container Toolkit
-    # Instructions copied from: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
     curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg >/dev/null &&
         curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |
         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
     sudo sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
     sudo apt-get update || true
     sudo apt-get install -y nvidia-container-toolkit
@@ -99,10 +99,43 @@ function install_nvidia_mig_manager() {
     popd
 }
 
+# Parse command-line arguments
+install_gpu_operator_flag=false
+install_nvidia_toolkit_flag=false
+install_nvidia_mig_manager_flag=false
+
+for arg in "$@"; do
+    case $arg in
+        --install-gpu-operator)
+        install_gpu_operator_flag=true
+        shift;;
+        --install-nvidia-toolkit)
+        install_nvidia_toolkit_flag=true
+        shift;;
+        --install-nvidia-mig-manager)
+        install_nvidia_mig_manager_flag=true
+        shift;;
+        *)
+        echo "Unknown option: $arg"
+        exit 1;;
+    esac
+done
+
 install_prereq_packages
 install_docker
 install_kind
 install_kubectl
 install_helm
-install_nvidia_toolkit
-install_nvidia_mig_manager
+
+# Install based on the parsed flags
+if [[ "$install_gpu_operator_flag" == true ]]; then
+    install_gpu_operator
+fi
+
+if [[ "$install_nvidia_toolkit_flag" == true ]]; then
+    install_nvidia_toolkit
+fi
+
+if [[ "$install_nvidia_mig_manager_flag" == true ]]; then
+    install_nvidia_mig_manager
+fi
